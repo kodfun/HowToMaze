157e64ef7a398592939d18c3b9ea1036a29da8ad
KodluYorum <kodluyorumtv@gmail.com>
2021-01-09 20:12:24 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace howto_maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.picMaze = new System.Windows.Forms.PictureBox();
+            this.txtMaze = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Width:";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(59, 12);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(35, 20);
+            this.txtWidth.TabIndex = 1;
+            this.txtWidth.Text = "10";
+            this.txtWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(158, 12);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(35, 20);
+            this.txtHeight.TabIndex = 3;
+            this.txtHeight.Text = "10";
+            this.txtHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height:";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(199, 10);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 4;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // picMaze
+            // 
+            this.picMaze.BackColor = System.Drawing.Color.White;
+            this.picMaze.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picMaze.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMaze.Location = new System.Drawing.Point(0, 0);
+            this.picMaze.Name = "picMaze";
+            this.picMaze.Size = new System.Drawing.Size(435, 497);
+            this.picMaze.TabIndex = 5;
+            this.picMaze.TabStop = false;
+            // 
+            // txtMaze
+            // 
+            this.txtMaze.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMaze.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaze.Location = new System.Drawing.Point(0, 0);
+            this.txtMaze.Multiline = true;
+            this.txtMaze.Name = "txtMaze";
+            this.txtMaze.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMaze.Size = new System.Drawing.Size(431, 497);
+            this.txtMaze.TabIndex = 6;
+            this.txtMaze.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Create Without Spaces";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 39);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.picMaze);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtMaze);
+            this.splitContainer1.Size = new System.Drawing.Size(870, 497);
+            this.splitContainer1.SplitterDistance = 435;
+            this.splitContainer1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(724, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Copy to Clipboard";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCreate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 548);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "How To Maze";
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.PictureBox picMaze;
+        private System.Windows.Forms.TextBox txtMaze;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+
+namespace howto_maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int Xmin, Ymin, CellWid, CellHgt;
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // Figure out the drawing geometry.
+            int wid = int.Parse(txtWidth.Text);
+            int hgt = int.Parse(txtHeight.Text);
+
+            CellWid = picMaze.ClientSize.Width / (wid + 2);
+            CellHgt = picMaze.ClientSize.Height / (hgt + 2);
+            if (CellWid > CellHgt) CellWid = CellHgt;
+            else CellHgt = CellWid;
+            Xmin = (picMaze.ClientSize.Width - wid * CellWid) / 2;
+            Ymin = (picMaze.ClientSize.Height - hgt * CellHgt) / 2;
+
+            // Build the maze nodes.
+            MazeNode[,] nodes = MakeNodes(wid, hgt);
+
+            // Build the spanning tree.
+            FindSpanningTree(nodes[0, 0]);
+
+            // Display the maze.
+            DisplayMaze(nodes);
+            DisplayMazeText(nodes);
+        }
+
+        // Make the network of MazeNodes.
+        private MazeNode[,] MakeNodes(int wid, int hgt)
+        {
+            // Make the nodes.
+            MazeNode[,] nodes = new MazeNode[hgt, wid];
+            for (int r = 0; r < hgt; r++)
+            {
+                int y = Ymin + CellHgt * r;
+                for (int c = 0; c < wid; c++)
+                {
+                    int x = Xmin + CellWid * c;
+                    nodes[r, c] = new MazeNode(
+                        x, y, CellWid, CellHgt, c , r);
+                }
+            }
+
+            // Initialize the nodes' neighbors.
+            for (int r = 0; r < hgt; r++)
+            {
+                for (int c = 0; c < wid; c++)
+                {
+                    if (r > 0)
+                        nodes[r, c].Neighbors[MazeNode.North] = nodes[r - 1, c];
+                    if (r < hgt - 1)
+                        nodes[r, c].Neighbors[MazeNode.South] = nodes[r + 1, c];
+                    if (c > 0)
+                        nodes[r, c].Neighbors[MazeNode.West] = nodes[r, c - 1];
+                    if (c < wid - 1)
+                        nodes[r, c].Neighbors[MazeNode.East] = nodes[r, c + 1];
+                }
+            }
+
+            // Return the nodes.
+            return nodes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnCreate.PerformClick();
+            txtMaze.Text = txtMaze.Text.Replace(" ", "");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtMaze.Text);
+        }
+
+        // Build a spanning tree with the indicated root node.
+        private void FindSpanningTree(MazeNode root)
+        {
+            Random rand = new Random();
+
+            // Set the root node's predecessor so we know it's in the tree.
+            root.Predecessor = root;
+
+            // Make a list of candidate links.
+            List<MazeLink> links = new List<MazeLink>();
+
+            // Add the root's links to the links list.
+            foreach (MazeNode neighbor in root.Neighbors)
+            {
+                if (neighbor != null)
+                    links.Add(new MazeLink(root, neighbor));
+            }
+
+            // Add the other nodes to the tree.
+            while (links.Count > 0)
+            {
+                // Pick a random link.
+                int link_num = rand.Next(0, links.Count);
+                MazeLink link = links[link_num];
+                links.RemoveAt(link_num);
+
+                // Add this link to the tree.
+                MazeNode to_node = link.ToNode;
+                link.ToNode.Predecessor = link.FromNode;
+
+                // Remove any links from the list that point
+                // to nodes that are already in the tree.
+                // (That will be the newly added node.)
+                for (int i = links.Count - 1; i >= 0; i--)
+                {
+                    if (links[i].ToNode.Predecessor != null)
+                        links.RemoveAt(i);
+                }
+
+                // Add to_node's links to the links list.
+                foreach (MazeNode neighbor in to_node.Neighbors)
+                {
+                    if ((neighbor != null) && (neighbor.Predecessor == null))
+                        links.Add(new MazeLink(to_node, neighbor));
+                }
+            }
+        }
+
+        // Display the maze in the picMaze PictureBox.
+        private void DisplayMaze(MazeNode[,] nodes)
+        {
+            int hgt = nodes.GetUpperBound(0) + 1;
+            int wid = nodes.GetUpperBound(1) + 1;
+            Bitmap bm = new Bitmap(
+                picMaze.ClientSize.Width,
+                picMaze.ClientSize.Height);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+                for (int r = 0; r < hgt; r++)
+                {
+                    for (int c = 0; c < wid; c++)
+                    {
+                        //nodes[r, c].DrawCenter(gr, Brushes.Red);
+                        nodes[r, c].DrawWalls(gr, Pens.Black);
+                        //nodes[r, c].DrawNeighborLinks(gr, Pens.Black);
+                        //nodes[r, c].DrawBoundingBox(gr, Pens.Blue);
+                        //nodes[r, c].DrawPredecessorLink(gr, Pens.LightGray);
+                    }
+                }
+            }
+
+            picMaze.Image = bm;
+        }
+
+        // Display the maze in the TextBox.
+        private void DisplayMazeText(MazeNode[,] nodes)
+        {
+            int hgt = nodes.GetLength(0);
+            int wid = nodes.GetLength(1);
+            string maze = "";
+
+            for (int y = 0; y < hgt; y++)
+            {
+                // mevcut hucre ve sağ duvar
+                for (int x = 0; x < wid; x++)
+                {
+                    MazeNode current = nodes[y, x];
+                    maze += "0 ";
+                    var east = current.Neighbors[MazeNode.East];
+                    if (east == null) continue;
+                    if (east.Predecessor == current || east == current.Predecessor)
+                    {
+                        maze += "0 ";
+                    }
+                    else
+                    {
+                        maze += "1 ";
+                    }
+                    // maze += current.ToString() + " ";
+                }
+                maze += Environment.NewLine;
+
+                // altduvar
+                for (int x = 0; x < wid; x++)
+                {
+                    MazeNode current = nodes[y, x];
+                    var south = current.Neighbors[MazeNode.South];
+                    if (south == null) continue;
+                    if (south.Predecessor == current || south == current.Predecessor)
+                    {
+                        maze += "0 1 ";
+                    }
+                    else
+                    {
+                        maze += "1 1 ";
+                    }
+                    // maze += current.ToString() + " ";
+                }
+                maze = maze.Remove(maze.Length - 2);
+                maze += Environment.NewLine;
+            }
+
+            txtMaze.Text = maze;
+        }
+    }
+}
